e79651840ed3aad1e1bc89cb685b58f486d091aa
XiaoBiniaoX彬彬 <xiaobinzhao1145@gmail.com>
2026-08-11 11:18:50 +0800

    Auto update src

--- src/main/java/bin/cnpcplus/craftingview/network/CraftingViewNetwork.java
+++ src/main/java/bin/cnpcplus/craftingview/network/CraftingViewNetwork.java
@@ -1,9 +1,12 @@
 package bin.cnpcplus.craftingview.network;
 
 import bin.cnpcplus.CnpcPlus;
+import bin.cnpcplus.invpage.network.PacketNpcInvPage;
 import bin.cnpcplus.recipe.network.PacketPersistState;
 import bin.cnpcplus.recipe.network.PacketQueryPersist;
 import bin.cnpcplus.recipe.network.PacketRecipePersist;
+import bin.cnpcplus.trader.network.PacketTraderPage;
+import bin.cnpcplus.trader.network.PacketTraderPageSync;
 import net.minecraftforge.fml.common.network.NetworkRegistry;
 import net.minecraftforge.fml.common.network.simpleimpl.SimpleNetworkWrapper;
 import net.minecraftforge.fml.relauncher.Side;
@@ -21,5 +24,8 @@ public final class CraftingViewNetwork {
         CHANNEL.registerMessage(PacketRecipePersist.Handler.class, PacketRecipePersist.class, nextId++, Side.SERVER);
         CHANNEL.registerMessage(PacketQueryPersist.Handler.class, PacketQueryPersist.class, nextId++, Side.SERVER);
         CHANNEL.registerMessage(PacketPersistState.Handler.class, PacketPersistState.class, nextId++, Side.CLIENT);
+        CHANNEL.registerMessage(PacketNpcInvPage.Handler.class, PacketNpcInvPage.class, nextId++, Side.SERVER);
+        CHANNEL.registerMessage(PacketTraderPage.Handler.class, PacketTraderPage.class, nextId++, Side.SERVER);
+        CHANNEL.registerMessage(PacketTraderPageSync.Handler.class, PacketTraderPageSync.class, nextId++, Side.CLIENT);
     }
 }
